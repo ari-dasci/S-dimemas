a68226fb82f3706bc772680e164ee003f4d359f5
Jeronimo Duran Curiel <Jeronimo-Duran@users.noreply.github.com>
2019-07-17 14:52:00 +0200

    Se estan haciendo las pruebas de generar un reporte desde RAW

--- reporte.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import numpy as np
-
-#Se carga el archivo csv con los datos de las evaluaciones
-datos = pd.read_csv('La_noche_2019_normalizado.csv', delimiter=',')
-#print(datos)
-
