07e3577e64b3a3b7f221c7a1ce20676bc63a029d
Jeronimo-Duran <jeronimoduran@correo.ugr.es>
2019-08-18 01:22:42 +0200

    Se está modificando el código para filtrar los datos para el informe

--- reporte_RAW.py
+++ reporte_RAW.py
@@ -5,30 +5,34 @@ from pylab import *
 import matplotlib.pyplot as plt
 
 #Se carga el archivo csv con los datos de las evaluaciones
-#Éste archivo ya está normalizado, solo está pendiente el paso para normalizar.
 datos1 = pd.read_csv('normalizados.csv', delimiter=',')
 #print(datos1)
 
-Total_evento = datos1['EVENTO'].count()
-#print(Total_evento)
+total_evento = datos1['EVENTO'].count()
+filtro01 = datos1.groupby(['EVENTO', 'GENERO'])['GENERO'].size().reset_index(name='TOTALES')
+#print(filtro01)
+
 
 conteo2 = datos1.iloc[:, :3]
-
 e = datos1['PROVINCIA'].value_counts()
 cont_prov = pd.DataFrame(e)
-print(cont_prov)
+filtro02 = datos1.groupby(['PROVINCIA','SUBEVENTO'])['SUBEVENTO'].size().reset_index(name='TOTALES')
+#print(filtro02)
 
 
 
 
-filtro03 = datos1.groupby(['PROVINCIA','SUBEVENTO','GENERO'])['GENERO'].count()
-filtro04 = datos1.groupby(['PROVINCIA','SUBEVENTO'])['SUBEVENTO'].count()
-filtro05 = datos1.groupby(['PROVINCIA','GENERO'])['GENERO'].count()
-filtro06 = datos1.groupby(['EVENTO', 'GENERO'])['GENERO'].count()
-print(filtro03)
-print(filtro04)
-print(filtro05)
-print(filtro06)
+filtro03 = datos1.groupby(['PROVINCIA','SUBEVENTO','GENERO'])['GENERO'].size().reset_index(name='TOTALES')
+filtro05 = datos1.groupby(['PROVINCIA','GENERO'])['GENERO'].size().reset_index(name='TOTALES')
+filtro06 = datos1.groupby(['EVENTO', 'GENERO'])['GENERO'].size().reset_index(name='TOTALES')
+#print(filtro03)
+#print(filtro04)
+#print(filtro05)
+#print(filtro06)
 
-a = pd.DataFrame(filtro03)
-print(a)
+conteo3 = datos1.iloc[:, :4]
+
+
+resultado = (conteo3.EDAD < 15).sum()
+
+print(resultado)
